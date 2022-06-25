9aea88586fa28586d6b1aec585a996c84a96ac7e
Jysai <jysai59@gmail.com>
2022-06-25 23:40:42 +0200

    affichage des filtres sans doublon

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 import { recipes } from "../data/recipes.js";
 
 const recipesSection = document.querySelector(".section-list-recipes");
+const arrayIngredientsForFilters = [];
+const arrayCookingtoolsForFilters = [];
+const arrayAppliancesForFilters = [];
 
 recipes.forEach((recipe) => {
   const article = document.createElement("article");
@@ -20,32 +23,44 @@ recipes.forEach((recipe) => {
   const recipeDescription = document.createElement("span");
   recipeDescription.classList.add("recipe-description");
 
-  const arrayIngredients = [];
+  const arrayIngredientsForArticle = [];
+
+  arrayAppliancesForFilters.push(recipe.appliance);
 
   recipe.ingredients.forEach((ingredients) => {
-    arrayIngredients.push(ingredients);
+    arrayIngredientsForArticle.push(ingredients);
+    arrayIngredientsForFilters.push(ingredients);
+  });
 
-    article.innerHTML = `     
+  recipe.ustensils.forEach((ustensils) => {
+    arrayCookingtoolsForFilters.push(ustensils);
+  });
+
+  article.innerHTML = `     
     <div class="picture-recipe"></div>
    `;
 
-    informationsRecipe.innerHTML = `<div class="name-and-timer"><h2 class="name-recipe">${recipe.name}</h2><span class="timer-recipe">${recipe.time} min</span></div>`;
 
-    recipeDescription.textContent = `${recipe.description}`;
-  });
+  
+
+  informationsRecipe.innerHTML = `<div class="name-and-timer"><h2 class="name-recipe">${recipe.name} </h2><span class="timer-recipe">${recipe.time}min </span></div>`;
+
+  recipeDescription.textContent = `${recipe.description}`;
 
   if (articleId == recipe.id) {
-    arrayIngredients.forEach((element) => {
+    arrayIngredientsForArticle.forEach((element) => {
       const ingredientsRecipe = document.createElement("span");
       ingredientsRecipe.classList.add("ingredients-recipe");
 
-      ingredientsRecipe.innerHTML = `<b>${element.ingredient}</b>${
+      ingredientsRecipe.innerHTML = `<b class="ingredient-name">${
+        element.ingredient
+      }</b>${
         element.quantity
           ? ": " + element.quantity
           : `${" "}` && element.quantite
           ? ": " + element.quantite
           : `${" "}`
-      } ${element.unit ? element.unit : `${" "}`}`;
+      } ${element.unit ? element.unit : `${" "}`} `;
 
       listIngredients.appendChild(ingredientsRecipe);
     });
@@ -56,4 +71,56 @@ recipes.forEach((recipe) => {
   informationsRecipe.appendChild(ingredientsAndDescriptions);
   article.appendChild(informationsRecipe);
   recipesSection.appendChild(article);
+});
+
+const arrayIngredientForDisplayFilters = [];
+const arrayCookingtoolsForDisplayFilters = [];
+
+arrayIngredientsForFilters.forEach((element) => {
+  arrayIngredientForDisplayFilters.push(element.ingredient.toLowerCase());
+});
+
+arrayCookingtoolsForFilters.forEach((element) => {
+  arrayCookingtoolsForDisplayFilters.push(element.toLowerCase());
+});
+
+const arrayCookingtoolsWithoutDuplicates = Array.from(
+  new Set(arrayCookingtoolsForDisplayFilters)
+);
+
+const arrayAppliancesWithoutDuplicates = Array.from(new Set(arrayAppliancesForFilters));
+
+const arrayIngredientsWithoutDuplicates = Array.from(
+  new Set(arrayIngredientForDisplayFilters)
+);
+
+const ingredientsFilterSection = document.querySelector(".ingredients-filter");
+const appliancesFilterSection = document.querySelector(".appliances-filter");
+const cookingtoolsFilterSection = document.querySelector(".ustensils-filter");
+
+arrayCookingtoolsWithoutDuplicates.forEach(element => {
+   const buttonCookingtools = document.createElement("button")
+   buttonCookingtools.classList.add("button-filter");
+
+   buttonCookingtools.innerHTML = element
+
+   cookingtoolsFilterSection.appendChild(buttonCookingtools)
+});
+
+arrayAppliancesWithoutDuplicates.forEach(element => {
+  const buttonCookingtools = document.createElement("button")
+  buttonCookingtools.classList.add("button-filter");
+
+  buttonCookingtools.innerHTML = element
+
+  appliancesFilterSection.appendChild(buttonCookingtools)
+});
+
+arrayIngredientsWithoutDuplicates.forEach(element => {
+  const buttonCookingtools = document.createElement("button")
+  buttonCookingtools.classList.add("button-filter");
+
+  buttonCookingtools.innerHTML = element
+
+  ingredientsFilterSection.appendChild(buttonCookingtools)
 });
--- scripts/search.js
+++ scripts/search.js
@@ -2,14 +2,23 @@ const articles = document.querySelectorAll(".article-recipe");
 const searchInput = document.getElementById("searchbox-input");
 
 searchInput.addEventListener("keyup", liveSearch);
-
 function liveSearch() {
   let searchQuery = searchInput.value;
 
   articles.forEach((article) => {
+    const articleName = article.querySelector(".name-recipe").innerHTML;
+    const articleDescription = article.querySelector(
+      ".recipe-description"
+    ).innerHTML;
+    const articleIngredient =
+      article.querySelector(".ingredient-name").innerHTML;
+
     if (searchQuery.length > 3) {
       if (
-        article.textContent.toLowerCase().includes(searchQuery.toLowerCase())
+        // console.log(article.textContent)
+        articleName.toLowerCase().match(searchQuery.toLowerCase()) ||
+        articleDescription.toLowerCase().match(searchQuery.toLowerCase()) ||
+        articleIngredient.toLowerCase().match(searchQuery.toLowerCase())
       ) {
         article.style.display = "block";
       } else {
